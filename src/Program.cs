f2dce8d669400d36d5da5fd0d9d98a25e5ca0c83
chenjn <chenjn@zotac>
2020-12-13 11:36:11 +0800

    code update

---
+++ src/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var found = false;
+            foreach (var p in Process.GetProcesses().OrderBy(p => p.ProcessName))
+            {
+                if (p.ProcessName.ToLower().Contains("trojan"))
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                var current_path = Process.GetCurrentProcess().MainModule?.FileName;
+                if (current_path != null)
+                {
+                    var psi = new ProcessStartInfo("trojan.exe")
+                    {
+                        WorkingDirectory = Path.GetDirectoryName(current_path) ?? ".",
+                        CreateNoWindow = true,
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                    };
+                    Process.Start(psi);
+                    Console.WriteLine("trojan started");
+                }
+                else
+                {
+                    Console.WriteLine("can not get cwd");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("trojan already running");
+            }
+        }
+    }
+}
