2c637c9a4b7d4564a962c78cec63cb9048073351
chenjn <chenjn@zotac>
2020-12-30 13:26:49 +0800

    web api experiment

---
+++ my-ip/Controllers/IpController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace my_ip.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IpController : ControllerBase
+    {
+        private readonly ILogger<IpController> _logger;
+
+        public IpController(ILogger<IpController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString();
+        }
+    }
+}
